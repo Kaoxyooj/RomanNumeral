0ceac98016353c554371273d15af117f81c011f7
Kaoxyooj <locksage@hotmail.com>
2016-01-14 18:05:17 -0600

    edited

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -15,19 +15,16 @@ function checksNumber(input,mymessage,roman){
 }
 
 function romanize(input) {
-	var	digits, key, myKey, roman,i;
-        digits = String(+input).split("");
-		key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
-		       "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
-		       "","I","II","III","IV","V","VI","VII","VIII","IX"],
-        myKey = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},       
-		roman = "",
-		i = 3;
-	while (i--)
-
-		roman = (key[+digits.pop() + (i * 10)] || "") + roman;
-	    roman = Array(+digits.join("") + 1).join("M") + roman;
-    return roman;
+  var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},
+      roman = '',
+      i;
+  for ( i in lookup ) {
+    while ( input >= lookup[i] ) {
+      roman += i;
+      input -= lookup[i];
+    }
+  }
+  return roman;
 }
         
 function deromanize(input) {
